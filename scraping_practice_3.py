df810399f907e28cd786cf38c5cffa2081397b07
kaseyuina <tomx2nakaji@gmail.com>
2023-05-01 23:36:20 +0900

    Multiple images can be retrieved

--- scraping_practice_3.py
+++ scraping_practice_3.py
@@ -8,11 +8,11 @@ res = requests.get(url)
 
 soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup)
-img_tag = soup.find('img')
-img_tag['src']
+for i, img_tag in enumerate(soup.find_all('img')):
+    # img_tag['src']
 
-root_url = 'https://scraping-for-beginner.herokuapp.com'
-img_url = root_url + img_tag['src']
+    root_url = 'https://scraping-for-beginner.herokuapp.com'
+    img_url = root_url + img_tag['src']
 
-img = Image.open(io.BytesIO(requests.get(img_url).content))
-img.save('img/sample.jpg')
+    img = Image.open(io.BytesIO(requests.get(img_url).content))
+    img.save(f'img/img{i}.jpg')
